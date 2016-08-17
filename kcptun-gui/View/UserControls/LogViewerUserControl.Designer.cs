20339c37e624760954139e8aa333cb4aedcd0665
Gang Zhuo <gang.zhuo@gmail.com>
2016-08-17 06:38:26 -0400

    update frequency to 300ms for log refresh

--- kcptun-gui/View/UserControls/LogViewerUserControl.Designer.cs
+++ kcptun-gui/View/UserControls/LogViewerUserControl.Designer.cs
@@ -53,7 +53,7 @@
             // timer1
             // 
             this.timer1.Enabled = true;
-            this.timer1.Interval = 1000;
+            this.timer1.Interval = 300;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // LogViewerUserControl
