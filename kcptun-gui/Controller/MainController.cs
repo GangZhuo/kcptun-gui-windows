8514c5a5a0c88be4e1b625c7f7c24e9b9aea97c6
Gang Zhuo <gang.zhuo@gmail.com>
2016-08-30 08:31:23 -0400

    bump 1.5

--- kcptun-gui/Controller/MainController.cs
+++ kcptun-gui/Controller/MainController.cs
@@ -11,7 +11,7 @@ namespace kcptun_gui.Controller
 {
     public class MainController
     {
-        public const string Version = "1.4";
+        public const string Version = "1.5";
 
         private IRelay _tcpRelay;
         private IRelay _udpRelay;
