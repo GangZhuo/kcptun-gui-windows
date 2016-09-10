f6010fdb6c9fa64fc87657ca151b612ba3aecc8b
Gang Zhuo <gang.zhuo@gmail.com>
2016-09-10 20:40:22 +0800

    detect os sleep and resume

--- kcptun-gui/Program.cs
+++ kcptun-gui/Program.cs
@@ -4,6 +4,7 @@ using System.Windows.Forms;
 
 using kcptun_gui.Controller;
 using kcptun_gui.View;
+using Microsoft.Win32;
 
 namespace kcptun_gui
 {
@@ -27,12 +28,29 @@ namespace kcptun_gui
                 MessageBox.Show(string.Format(I18N.GetString("Can't access the file '{0}', it is maybe used by another process."), Logging.LogFilePath),
                     I18N.GetString("Error"), MessageBoxButtons.OK, MessageBoxIcon.Warning);
             }
-
             controller = new MainController();
             MenuViewController viewController = new MenuViewController(controller);
             controller.Start();
             Application.ApplicationExit += Application_ApplicationExit;
+            SystemEvents.PowerModeChanged += SystemEvents_PowerModeChanged;
             Application.Run();
+        }
+
+        private static void SystemEvents_PowerModeChanged(object sender, PowerModeChangedEventArgs e)
+        {
+            switch (e.Mode)
+            {
+                case PowerModes.Resume:
+                    Console.WriteLine("os wake up");
+                    if (controller != null)
+                        controller.Start();
+                    break;
+                case PowerModes.Suspend:
+                    if (controller != null)
+                        controller.Stop();
+                    Console.WriteLine("os suspend");
+                    break;
+            }
         }
 
         private static void Application_ApplicationExit(object sender, EventArgs e)
